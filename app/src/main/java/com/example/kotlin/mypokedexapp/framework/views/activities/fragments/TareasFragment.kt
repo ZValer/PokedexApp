25ece51c2722024da62e9f98101bc29190311ffe
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-09-21 21:09:11 -0600

    Hacer que btn_busqueda lleve al SearchFragment sin  lógica de MainActivity (conflicto Navbar)

--- app/src/main/java/com/example/kotlin/mypokedexapp/framework/views/activities/fragments/TareasFragment.kt
+++ app/src/main/java/com/example/kotlin/mypokedexapp/framework/views/activities/fragments/TareasFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.kotlin.mypokedexapp.R
+import com.example.kotlin.mypokedexapp.framework.views.activities.fragments.SearchFragment
 
 class TareasFragment : Fragment() {
 
@@ -14,6 +16,13 @@ class TareasFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflar el layout del fragmento
-        return inflater.inflate(R.layout.fragment_tareas, container, false)
+       val view = inflater.inflate(R.layout.fragment_tareas, container, false)
+        view.findViewById<Button>(R.id.btn_busqueda).setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment_content_main, SearchFragment())
+                .addToBackStack(null) // Esto permite volver al TareasFragment
+                .commit()
+        }
+        return view
     }
 }
