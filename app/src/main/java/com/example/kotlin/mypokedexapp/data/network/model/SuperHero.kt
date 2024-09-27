c0cc27106cfdcbca91b77854ee0a53d9b76ab72b
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-09-26 22:38:15 -0600

    Crear data class y lista de superheroes

---
+++ app/src/main/java/com/example/kotlin/mypokedexapp/data/network/model/SuperHero.kt
@@ -0,0 +1,9 @@
+package com.example.kotlin.mypokedexapp.data.network.model
+
+// Clase para mi lista de tareas
+data class SuperHero(
+    val superhero:String,
+    val publisher:String,
+    val realName:String,
+    val photo: String
+)
