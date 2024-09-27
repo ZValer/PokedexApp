a6ec100bf05ad186435a7db72f8ab5c57805aad8
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-09-27 12:46:55 -0600

    SetOnClickListeners dentro de View Holder para desplegar nombres

--- app/src/main/java/com/example/kotlin/mypokedexapp/framework/adapters/viewholders/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/kotlin/mypokedexapp/framework/adapters/viewholders/SuperHeroViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.kotlin.mypokedexapp.framework.adapters.viewholders
 
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.kotlin.mypokedexapp.data.network.model.SuperHero
@@ -14,5 +15,21 @@ class SuperHeroViewHolder (val view: View): RecyclerView.ViewHolder(view){
         binding.tvRealName.text = superHeroModel.realName
         binding.tvPublisher.text = superHeroModel.publisher
         Glide.with(binding.ivSuperHero.context).load(superHeroModel.photo).into(binding.ivSuperHero)
+
+        // Click en la imagen para desplegar nombre real
+        binding.ivSuperHero.setOnClickListener {
+            Toast.makeText(
+                binding.ivSuperHero.context,
+                superHeroModel.realName,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        // Click en el item para desplegar nombre
+        itemView.setOnClickListener {Toast.makeText(
+            binding.ivSuperHero.context,
+            superHeroModel.superhero,
+            Toast.LENGTH_SHORT
+        ).show() }
     }
 }
